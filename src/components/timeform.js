d551f906a80743fda20fa4b1382620b3b28a0f5b
Stan <slim679975@gmail.com>
2017-08-12 22:45:13 -0400

    Started adding options menu in time form

--- src/components/timeform.js
+++ src/components/timeform.js
@@ -20,55 +20,80 @@ class TimeForm extends Component {
     // TODO: Add 12-hour and 24-hour formats
     render() {
         return (
-            <div className="row level fluid-container">
-                <div className="col-fluid input-control">
-                    <select
-                        id='hour'
-                        onChange={ (e) => this.setHours(e.target.value) }
-                    >
-                        <option>(hours)</option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>6</option>
-                        <option>7</option>
-                        <option>8</option>
-                        <option>9</option>
-                        <option>10</option>
-                        <option>11</option>
-                        <option>12</option>
-                    </select>
+            <div>
+                <div className="row level fluid-container">
+                    <div className="col-fluid input-control">
+                        <select
+                            id='hour'
+                            onChange={ (e) => this.setHours(e.target.value) }
+                        >
+                            <option>(hours)</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                            <option>9</option>
+                            <option>10</option>
+                            <option>11</option>
+                            <option>12</option>
+                        </select>
+                    </div>
+                    <div className="col-fluid input-control">
+                        <select
+                            id='minute'
+                            onChange={ (e) => this.setMinutes(e.target.value) }
+                        >
+                            <option>(minutes)</option>
+                            <option>00</option>
+                            <option>05</option>
+                            <option>10</option>
+                            <option>15</option>
+                            <option>20</option>
+                            <option>25</option>
+                            <option>30</option>
+                            <option>35</option>
+                            <option>40</option>
+                            <option>45</option>
+                            <option>50</option>
+                            <option>55</option>
+                        </select>
+                    </div>
+                    <div className="col-fluid input-control">
+                        <select
+                            id='ampm'
+                            onChange={ (e) => this.setAMPM(e.target.value) }
+                        >
+                            <option>AM</option>
+                            <option>PM</option>
+                        </select>
+                    </div>
                 </div>
-                <div className="col-fluid input-control">
-                    <select
-                        id='minute'
-                        onChange={ (e) => this.setMinutes(e.target.value) }
-                    >
-                        <option>(minutes)</option>
-                        <option>00</option>
-                        <option>05</option>
-                        <option>10</option>
-                        <option>15</option>
-                        <option>20</option>
-                        <option>25</option>
-                        <option>30</option>
-                        <option>35</option>
-                        <option>40</option>
-                        <option>45</option>
-                        <option>50</option>
-                        <option>55</option>
-                    </select>
+                <div className="center">
+                    <button id="btnMore">More Options</button>
                 </div>
-                <div className="col-fluid input-control">
-                    <select
-                        id='ampm'
-                        onChange={ (e) => this.setAMPM(e.target.value) }
-                    >
-                        <option>AM</option>
-                        <option>PM</option>
-                    </select>
+                <div className="content" id="moreOptions">
+                    <h6>More Options</h6>
+                    <div className="form-group">
+                        <label className="form-group-label">Time to fall asleep</label>
+                        <input className="form-group-input" type="number" min="0" defaultValue="14" />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-group-label">Average Sleep Cycle</label>
+                        <select className="select form-group-input" defaultValue="90">
+                            <option>81</option>
+                            <option>84</option>
+                            <option>87</option>
+                            <option>90</option>
+                            <option>93</option>
+                            <option>96</option>
+                            <option>99</option>
+                            <option>102</option>
+                        </select>
+                    </div>
                 </div>
             </div>
         );
