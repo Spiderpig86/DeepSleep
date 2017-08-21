822864ce4b2da076c853267df27fdafe2f6cb8af
Stan <slim679975@gmail.com>
2017-08-21 13:02:36 -0400

    feat: Finishing up theming for app

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Tabs from './tabs';
-import '../styles/css/cirrus.min.css'; // Need to import to use stylesheet
 import './tabcontrol.js'; // Import the tabs library
 import { setUpTabs } from '../tabloader.js'; // Import the JS to bind the tabs
+
+import '../styles/dist/cirrus.min.css'; // Need to import to use stylesheet
 import '../styles/dist/app.min.css';
+import '../styles/dist/scroll.min.css';
 
 class App extends Component {
     // Bind the tab event handler after the component renders
